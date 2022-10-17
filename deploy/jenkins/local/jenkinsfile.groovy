5230782ab54c8716f0856db706a930d9e99e433e
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-10-17 19:30:42 -0300

    configuring docker jenkinsfile

--- deploy/jenkins/local/jenkinsfile.groovy
+++ deploy/jenkins/local/jenkinsfile.groovy
@@ -16,22 +16,22 @@
                      sh "echo $deploy"
                  }
              }
-//             stage('clean') {
-//                 steps {
-//                     sh 'chmod +x gradlew'
-//                     sh './gradlew clean'
-//                 }
-//             }
-//             stage('test') {
-//                 steps {
-//                     sh './gradlew test'
-//                 }
-//             }
-//             stage('build') {
-//                 steps {
-//                     sh './gradlew build'
-//                 }
-//             }
+             stage('clean') {
+                 steps {
+                     sh 'chmod +x gradlew'
+                     sh './gradlew clean'
+                 }
+             }
+             stage('test') {
+                 steps {
+                     sh './gradlew test'
+                 }
+             }
+             stage('build') {
+                 steps {
+                     sh './gradlew build'
+                 }
+             }
              stage('deploy') {
                  steps {
                      sh "docker compose -f ./deploy/docker/docker-compose-docker.yml up -d"
