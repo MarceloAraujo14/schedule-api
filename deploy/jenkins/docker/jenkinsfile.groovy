2a1f77c2a55928a75bc9c4dc080370d05f32804b
marceloaraujo14 <mbaraujo1406@gmail.com>
2022-10-17 16:45:11 -0300

    alter jenkinsfile

--- deploy/jenkins/docker/jenkinsfile.groovy
+++ deploy/jenkins/docker/jenkinsfile.groovy
@@ -1,51 +1,51 @@
-#!groovy
+#!/bin/bash
 
-def startPipe(){
-    sh "echo $selectBranch"
-}
-// pipeline {
-//     agent none
-//
-//     environment {
-//              branch = '$selectBranch'
-//              deploy = '$deployEnv'
-//         }
-//
-//         stages {
-//             stage('checkout') {
-//                 steps {
-//                     sh "git checkout \"$branch\""
-//                     sh "echo \"$branch\""
-//                 }
-//             }
-//             stage('clean') {
-//                 steps {
-//                     sh 'chmod +x gradlew'
-//                     sh './gradlew clean'
-//                 }
-//             }
-//             stage('test') {
-//                 steps {
-//                     sh './gradlew test'
-//                 }
-//             }
-//             stage('build') {
-//                 steps {
-//                     sh './gradlew build'
-//                 }
-//             }
-//             stage('buildImage') {
-//                 steps {
-//         //                 sh './gradlew :bootBuildImage'
-//                         sh "echo 'buildando imagem'"
-//                 }
-//             }
-//             stage('deploy') {
-//                 steps {
-//                         sh "echo fazendo deploy"
-//         //                 sh 'cd deploy/docker'
-//         //                 sh 'docker-compose up -d'
-//                 }
-//             }
-//     }
-// }
+//def startPipe(){
+//    sh "echo $selectBranch"
+//}
+ pipeline {
+     agent any
+
+     environment {
+              branch = '$selectBranch'
+              deploy = '$deployEnv'
+         }
+
+         stages {
+             stage('checkout') {
+                 steps {
+                     sh "git checkout \"$branch\""
+                     sh "echo \"$branch\""
+                 }
+             }
+             stage('clean') {
+                 steps {
+                     sh 'chmod +x gradlew'
+                     sh './gradlew clean'
+                 }
+             }
+             stage('test') {
+                 steps {
+                     sh './gradlew test'
+                 }
+             }
+             stage('build') {
+                 steps {
+                     sh './gradlew build'
+                 }
+             }
+             stage('buildImage') {
+                 steps {
+         //                 sh './gradlew :bootBuildImage'
+                         sh "echo 'buildando imagem'"
+                 }
+             }
+             stage('deploy') {
+                 steps {
+                         sh "echo fazendo deploy"
+         //                 sh 'cd deploy/docker'
+         //                 sh 'docker-compose up -d'
+                 }
+             }
+     }
+ }
